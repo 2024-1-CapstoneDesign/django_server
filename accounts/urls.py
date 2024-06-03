ec9045a832ae50b607733b1e3071af4e91b5e0f2
hayeongKo <polarpheno@icloud.com>
2024-06-03 13:28:52 +0900

    원복

---
+++ accounts/urls.py
@@ -0,0 +1,18 @@
+from .views import *
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path("google/login/", google_login, name="google_login"),
+    path("google/callback/", google_callback, name="google_callback"),
+    path("google/callback/re/", google_callback_re, name="google_callback"),
+
+
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
