5d5220302fea902226129c9a97947c90ed6ecc81
hayeongKo <polarpheno@icloud.com>
2024-06-04 13:36:51 +0900

    redirect uri 수정

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 # Create your models here.
 class User(AbstractUser):
+    id = models.AutoField(primary_key=True, db_column='member_id')
     name = models.CharField(max_length=254, null=True, blank=True)
     picture = models.CharField(max_length=254, null=True, blank=True)
 
