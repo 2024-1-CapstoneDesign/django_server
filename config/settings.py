e8e553299b574ba27ba7f9073dda4fb6b00fa0c9
hayeongKo <polarpheno@icloud.com>
2024-11-30 04:12:46 +0900

    auth 추가

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ CORS_ALLOWED_ORIGINS = [
 
 DJANGO_APPS = [
     'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
