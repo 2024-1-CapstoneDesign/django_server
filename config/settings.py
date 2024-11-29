068e0846d7e2d79e7af75563e14648c52436243c
hayeongKo <polarpheno@icloud.com>
2024-11-30 04:14:50 +0900

    allauth 제거

--- config/settings.py
+++ config/settings.py
@@ -71,8 +71,7 @@ MIDDLEWARE = [
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "allauth.account.middleware.AccountMiddleware",  
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
